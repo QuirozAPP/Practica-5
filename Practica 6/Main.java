bace077f46a57c6ecc148fa61525a35047c46864
QuirozAPP <a1177095@uabc.edu.mx>
2024-09-26 00:27:00 -0700

    Main.java
    1.1

--- Practica 6/Main.java
+++ Practica 6/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        Square square = new Square();
-        square.changeColor("blue");
-        square.makeVisible();
-        Circle circle = new Circle();
-        circle.changeColor("red");
-        circle.makeVisible();
-        Person person = new Person();
-        person.changeColor("green");
-        person.makeVisible();
+
+        Carta carta1 = new Carta("A", Palo.CORAZONES);
+
+        Visualizador.carta(carta1, new Posicion(20, 40));
+
+        Carta carta2 = new Carta("K", Palo.PICAS);
+
+        Visualizador.carta(carta2, new Posicion(150, 40));
     }
 }
+
