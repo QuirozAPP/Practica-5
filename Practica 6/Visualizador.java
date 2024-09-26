d1b73a3b5fb4d98ad8463a06d90684e61d6adecd
QuirozAPP <a1177095@uabc.edu.mx>
2024-09-26 00:08:10 -0700

    Visualizador.java
    .

--- Practica 6/Visualizador.java
+++ Practica 6/Visualizador.java
@@ -1,21 +1,19 @@
 public class Visualizador {
 
     public static void carta(Carta c, Posicion p) {
-        // Crear un cuadrado para representar la carta
+
         Square cartaShape = new Square();
         cartaShape.changeColor("white");
-        cartaShape.changeSize(100);  // tamaño estándar de la carta
+        cartaShape.changeSize(100);
         cartaShape.moveHorizontal(p.x);
         cartaShape.moveVertical(p.y);
         cartaShape.makeVisible();
 
-        // Crear un círculo para representar el palo de la carta
         Circle paloShape = new Circle();
-        paloShape.changeSize(30);  // tamaño más pequeño para el palo
-        paloShape.moveHorizontal(p.x + 35);  // ajustar posición del círculo
+        paloShape.changeSize(30); 
+        paloShape.moveHorizontal(p.x + 35);
         paloShape.moveVertical(p.y + 35);
 
-        // Cambiar el color dependiendo del palo
         switch (c.palo) {
             case CORAZONES:
                 paloShape.changeColor("red");
@@ -32,10 +30,9 @@ public class Visualizador {
         }
         paloShape.makeVisible();
 
-        // Mostrar el valor de la carta (usando la clase Person como placeholder para el valor)
         Person valorDisplay = new Person();
         valorDisplay.changeColor("blue");
-        valorDisplay.moveHorizontal(p.x + 50);  // ajustar posición del valor
+        valorDisplay.moveHorizontal(p.x + 50);
         valorDisplay.moveVertical(p.y + 70);
         valorDisplay.makeVisible();
     }
